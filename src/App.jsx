2a9d0338d88e769f0040418cee95a34f033118ac
Jhonny Callisaya <jhonnyeddy.22@gmail.com>
2023-04-04 20:39:27 -0400

    add list chat

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import Header from './components/Header';
+import ChatList from './components/ListChat/ListChat';
+import PageView from './components/PageView/PageView';
 
 function App() {
 
   return (
     <div className="App">
       <Header/>
+      <div className='main-container'>
+        <ChatList/>
+        <PageView/>
+      </div>
     </div>
   )
 }
